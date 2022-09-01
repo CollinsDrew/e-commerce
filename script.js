aa25cbcd877a664fad75b2c0fd87e95580414613
CollinsDrew <collinsdrew1990@gmail.com>
2022-09-01 20:24:53 +0100

    graph added

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ let cricketBatElement = document.getElementById('cricketBatAnalytics');
 tennisBatElement.innerHTML = tennisBatCounter;
 cricketBatElement.innerHTML = cricketBatCounter;
 
+// Hover Analytics
 function tennisBatHover() {
     tennisBatCounter++;
     tennisBatElement.innerHTML = tennisBatCounter;
@@ -18,3 +19,31 @@ function cricketBatHover() {
     cricketBatCounter++;
     cricketBatElement.innerHTML = cricketBatCounter;
 }
+
+//Canvas chart
+
+const labels = [
+    'Tennis Bat',
+    'Cricket Bat',
+  ];
+
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'Hover Analytics',
+      backgroundColor: ['rgb(255, 99, 132)','rgb(255, 99, 132)'],
+      borderColor: 'rgb(255, 99, 132)',
+      data: [10, 10],
+    }]
+  };
+
+  const config = {
+    type: 'bar',
+    data: data,
+    options: {}
+  };
+
+  const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+  );
