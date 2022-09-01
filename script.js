aa6c250dc01a1d6cc0b25a4015c59ab6cbe9501a
CollinsDrew <collinsdrew1990@gmail.com>
2022-09-01 19:43:07 +0100

    cart analytics added

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 'use strict'
+
+let tennisBatCounter = 0;
+let cricketBatCounter =0;
+
+let tennisBatElement = document.getElementById('tennisBatAnalytics');
+let cricketBatElement = document.getElementById('cricketBatAnalytics');
+
+tennisBatElement.innerHTML = tennisBatCounter;
+cricketBatElement.innerHTML = cricketBatCounter;
+
+function tennisBatHover() {
+    tennisBatCounter++;
+    tennisBatElement.innerHTML = tennisBatCounter;
+}
+
+function cricketBatHover() {
+    cricketBatCounter++;
+    cricketBatElement.innerHTML = cricketBatCounter;
+}
